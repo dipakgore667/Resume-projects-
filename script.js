bf9a12bf551ebe7ba0d4f4400470b7bda751fea4
dipakgore667 <113288524+dipakgore667@users.noreply.github.com>
2023-05-15 18:53:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.sunimg("dark-mode");
+}
+
+function myFunction() {
+    document.getElementById("github").innerHTML = "";
+}
